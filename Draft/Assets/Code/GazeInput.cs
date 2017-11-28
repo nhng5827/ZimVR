5377963ab34eba4046e10570c1011ad0d51fff13
nhng5827 <nhng5827@colorado.edu>
2017-11-28 16:09:50 -0700

    add assets

---
+++ Draft/Assets/Code/GazeInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class GazeInput : MonoBehaviour
+{
+	private Ray ray;
+	private RaycastHit hitInfo;
+
+	[SerializeField]
+	[Tooltip("The root object of all menu items")]
+	private GameObject menuRoot;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (menuRoot.activeInHierarchy == false)
+			{
+				ShowMenu();
+			}
+			else
+			{
+				TrySelectMenuItem();
+			}
+		}
+	}
+
+	private void TrySelectMenuItem()
+	{
+		ray = new Ray(transform.position, transform.forward);
+		if (Physics.Raycast(ray, out hitInfo))
+		{
+			var videoSelection = hitInfo.collider.GetComponent<VideoSelection>();
+			if (videoSelection != null)
+			{
+				videoSelection.StartClipIfNotAlreadyPlaying();
+				HideMenu();
+			}
+		}
+		else
+		{
+			HideMenu();
+		}
+	}
+
+	private void ShowMenu()
+	{
+		menuRoot.SetActive(true);
+	}
+
+	private void HideMenu()
+	{
+		menuRoot.SetActive(false);
+	}
+}
---
+++ Draft/Assets/Code/VideoSelection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoSelection : MonoBehaviour
+{
+	[SerializeField]
+	private VideoClip videoClip;
+
+	private VideoPlayer videoPlayer;
+
+	private void Start()
+	{
+		videoPlayer = FindObjectOfType<VideoPlayer>();
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		StartClipIfNotAlreadyPlaying();
+	}
+
+	public void StartClipIfNotAlreadyPlaying()
+	{
+		if (videoPlayer.clip != videoClip)
+		{
+			videoPlayer.clip = videoClip;
+			videoPlayer.Play();
+		}
+	}
+}
